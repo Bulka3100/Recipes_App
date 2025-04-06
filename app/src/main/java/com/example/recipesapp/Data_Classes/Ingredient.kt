16e2c3395c0f9cefa397b67137f32a50c0654a2b
Breaker <kirillvip3100@yandex.ru>
2025-04-06 20:46:50 +0300

    RA-12

--- app/src/main/java/com/example/recipesapp/Data_Classes/Ingredient.kt
+++ app/src/main/java/com/example/recipesapp/Data_Classes/Ingredient.kt
@@ -1,7 +1,11 @@
 package com.example.recipesapp.Data_Classes
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Ingredient(
     val quantity: String,
     val unitOfMeasure: String,
     val description: String,
-)
+):Parcelable
--- app/src/main/java/com/example/recipesapp/RecipeFragment.kt
+++ app/src/main/java/com/example/recipesapp/RecipeFragment.kt
@@ -1,14 +1,17 @@
 package com.example.recipesapp
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recipesapp.Data_Classes.Recipe
+import com.example.recipesapp.RecipesListFragment.Companion.ARG_RECIPE
 import com.example.recipesapp.databinding.FragmentFavoritesBinding
 import com.example.recipesapp.databinding.RecipeFragmentBinding
 
-class RecipeFragment :Fragment(){
+class RecipeFragment : Fragment() {
     private var _binding: RecipeFragmentBinding? = null
     private val binding get() = _binding ?: throw IllegalStateException("пустой фрагмент")
     override fun onCreateView(
@@ -16,7 +19,29 @@ class RecipeFragment :Fragment(){
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = RecipeFragmentBinding.inflate(inflater,container,false)
+        _binding = RecipeFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
+// нужно ли было с let заморачиваться вообще?
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recipe = arguments?.let {
+            if (Build.VERSION.SDK_INT >= 33) {
+                it.getParcelable(ARG_RECIPE, Recipe::class.java)
+            } else {
+                @Suppress("DEPRECATION")
+                it.getParcelable(ARG_RECIPE) as? Recipe
+            }
+        }
+        recipe?.let {
+            binding.tvId.text = it.title
+
+        } ?: run{
+            binding.tvId.text = "рецепт не найдет"
+        }
+
+    }
+
 }
+
+
