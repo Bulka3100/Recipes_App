cff3f49ddd741e66a765c287e159514d78d794e4
Breaker <kirillvip3100@yandex.ru>
2025-04-18 00:27:32 +0300

    RA-14-corrected

--- app/src/main/java/com/example/recipesapp/IngredientsAdapter.kt
+++ app/src/main/java/com/example/recipesapp/IngredientsAdapter.kt
@@ -1,39 +1,43 @@
 package com.example.recipesapp
 
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipesapp.Data_Classes.Ingredient
 import com.example.recipesapp.databinding.ItemIngredientsBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class IngredientsAdapter(private val dataSet: List<Ingredient>) :
     RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
-    fun updateIngredients(progress: Int) {
-        quantity = progress
-    }
 
     var quantity: Int = 1
+
+    fun updateIngredients(progress: Int) {
+        quantity = progress
+        notifyDataSetChanged()
+    }
 
     class ViewHolder(val binding: ItemIngredientsBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(ingredient: Ingredient, quantity: Int) {
             ingredientTextView.text = ingredient.description
-            val amount = ingredient.quantity.toIntOrNull() ?: 0
-            val newAmount = (amount * quantity)
-            //не думаю что лучший вариант
-            val newAmountFormated = if (newAmount % 1.0 == 0.0) {
-                newAmount.toString()
-            } else {
-                String.format("%.1f", newAmount)
-            }
-            quantityTextView.text = newAmountFormated
+         // вроде такая запись адекватно выглядит. Подойдет такой вариант?
+            val amount = ingredient.quantity.toBigDecimalOrNull() ?: BigDecimal.ZERO
+
+            val totalQuantity = amount * BigDecimal(quantity)
+            val displayQuantity = totalQuantity
+                .setScale(1, RoundingMode.HALF_UP)
+                .stripTrailingZeros()
+                .toPlainString()
+
+
+            quantityTextView.text = displayQuantity
             unitOfMeasureTextView.text = ingredient.unitOfMeasure
         }
 
         val ingredientTextView = binding.tvIngridient
         val quantityTextView = binding.tvQuantity
         val unitOfMeasureTextView = binding.tvUnit
-//        а зачем так делать?
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -44,7 +48,6 @@ class IngredientsAdapter(private val dataSet: List<Ingredient>) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val ingredient = dataSet[position]
-        //вот так вроде неплохо получилось, спасибо)
         holder.bind(ingredient, quantity)
     }
 
