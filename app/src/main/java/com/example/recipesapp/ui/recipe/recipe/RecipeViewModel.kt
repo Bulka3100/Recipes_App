9964dcc79460a10d43a4766931ec4626970baded
Breaker <kirillvip3100@yandex.ru>
2025-05-06 07:25:05 +0300

    RA-23

--- app/src/main/java/com/example/recipesapp/ui/recipe/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipesapp/ui/recipe/recipe/RecipeViewModel.kt
@@ -1,13 +1,25 @@
 package com.example.recipesapp.ui.recipe.recipe
 
+import android.app.Application
+import android.content.Context
 import android.util.Log
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.recipesapp.R
+import com.example.recipesapp.data.STUB
 import com.example.recipesapp.model.Recipe
+import com.example.recipesapp.ui.recipe.recipe.RecipeFragment.Companion.KEY_FAVORITES
+import com.example.recipesapp.ui.recipe.recipe.RecipeFragment.Companion.PREFS_NAME
 
-class RecipeViewModel : ViewModel() {
+class RecipeViewModel : AndroidViewModel(application = Application()) {
     private val savedState: MutableLiveData<RecipeUiState>? = null
+    private val sharedPrefs by lazy {
+        //проблема с видимостью
+        application.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+    }
 
     // плохо понимаю зачем тут нужно две переменных смысл? посню с binding так делали, там вроде понятно, могла ошибка высветиться. Ксати тут надо обрабатывать ошибку с элвисом? вроде не может быть null а если надо , то как? спасибо
     val _savedState: LiveData<RecipeUiState>
@@ -22,5 +34,40 @@ class RecipeViewModel : ViewModel() {
     init {
         savedState?.value = RecipeUiState(isFavorite = false)
         Log.i("!!!", "initialized")
+
+    }
+
+    fun loadRecipe(recipeId: Int) {
+//        TODO  'load from network'
+        val recipe = STUB.getRecipeById(recipeId)
+
+        val isFavorite = recipeId.toString() in getFavorites()
+        savedState?.value = RecipeUiState(
+            recipe = recipe,
+            isFavorite = isFavorite,
+            //как взять значение для порций из текущего стйта? так можно?
+            portionsCount = RecipeUiState().portionsCount
+        )
+
+    }
+
+    private fun getFavorites(): MutableSet<String> {
+        return HashSet(sharedPrefs.getStringSet(KEY_FAVORITES, emptySet<String>()) ?: emptySet())
+    }
+
+    private fun saveFavorites(set: Set<String>) {
+        sharedPrefs.edit().putStringSet(KEY_FAVORITES, set).commit()
+    }
+
+    fun onFavoriteClicked(recipeId: Int) {
+
+        val updatedFavorites = getFavorites()
+        if (updatedFavorites.contains(recipeId.toString())) {
+            updatedFavorites.remove(recipeId.toString())
+        } else {
+            updatedFavorites.add((recipeId.toString()))
+        }
+
+        saveFavorites(updatedFavorites)
     }
 }
