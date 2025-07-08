239ff204be3a21f4260c4861850ab46c421cfe74
Bulka3100 <kirillvip3100@yandex.ru>
2025-07-08 06:34:48 +0300

    RA-31

--- app/src/main/java/com/example/recipesapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/ui/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.recipesapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.navigation.findNavController
 import com.example.recipesapp.R
 import com.example.recipesapp.databinding.ActivityMainBinding
+import com.example.recipesapp.model.Category
 import com.example.recipesapp.ui.categories.CategoriesListFragment
 import com.example.recipesapp.ui.recipe.favorites.FavoritesFragment
+import kotlinx.serialization.json.Json
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
@@ -20,10 +26,26 @@ class MainActivity : AppCompatActivity() {
         _binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        Log.i("!!!", "Метод onCreate() выполняется на потоке: ${Thread.currentThread().name}")
+        val thread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+            val json = connection.inputStream.bufferedReader().readText()
+            Log.i("!!!", "Выполняю запрос на потоке : ${Thread.currentThread().name}")
+            Log.i("!!!", "responseCode: ${connection.responseCode}")
+            Log.i("!!!", "responseMessage: ${connection.responseCode}")
+            Log.i("!!!", "body: ${json}")
+            println(connection.inputStream.bufferedReader().readText())
+            val deserealized = Json.decodeFromString<List<Category>>(json)
+        }
+        thread.start()
+
+
 
 
         binding.btnFavorites.setOnClickListener {
-findNavController(R.id.nav_host_fragment).navigate(R.id.favoritesFragment)
+            findNavController(R.id.nav_host_fragment).navigate(R.id.favoritesFragment)
 
         }
 
