9743d826167fc39421ee0cafa1850fd7c33f35ea
Bulka3100 <kirillvip3100@yandex.ru>
2025-12-06 18:17:55 +0300

    RA-41

--- app/src/main/java/com/example/recipesapp/ui/recipe/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/example/recipesapp/ui/recipe/favorites/FavoritesViewModel.kt
@@ -1,12 +1,9 @@
 package com.example.recipesapp.ui.recipe.favorites
 
 import android.app.Application
-import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.recipesapp.KEY_FAVORITES
-import com.example.recipesapp.PREFS_NAME
 import com.example.recipesapp.data.repository.RecipesRepository
 import com.example.recipesapp.model.Recipe
 import kotlinx.coroutines.launch
@@ -15,29 +12,14 @@ class FavoritesViewModel(application: Application) : AndroidViewModel(applicatio
     private val _uiState = MutableLiveData(FavoritesUiState())
     val uiState = _uiState
     val repository = RecipesRepository(application)  // ← ПЕРЕДАЙ application!
-    val sharedPrefs by lazy {
-        application.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-    }
-
     data class FavoritesUiState(
         val recipes: List<Recipe> = emptyList()
     )
 
     fun loadFavorites() {
         viewModelScope.launch {
-            val favorites = getFavorites()
-            val favoriteIds = favorites.mapNotNull { it.toIntOrNull() }
-            val result = repository.getRecipesByIds(favoriteIds)
-            val safeFavoriteRecipes = when (result) {
-                is RecipesRepository.ApiResult.Success -> result.data
-                is RecipesRepository.ApiResult.Failure -> emptyList()
-            }
-            _uiState.value = FavoritesUiState(recipes = safeFavoriteRecipes)
+            val favorites = repository.getFavoriteRecipes()
+            _uiState.value = FavoritesUiState(recipes = favorites)
         }
-    }
-
-    fun getFavorites(): MutableSet<String> {
-        val getId = sharedPrefs.getStringSet(KEY_FAVORITES, emptySet<String>())
-        return HashSet(getId ?: emptySet())
     }
 }
